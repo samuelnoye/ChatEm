f077f5b0128f9891b11bef48f4d725a74c1cac35
Noye Samuel <samuel.noye@amalitech.org>
2022-11-14 15:43:26 +0000

    Initial commit

---
+++ ChatEm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatEm
+//
+//  Created by Noye Samuel on 14/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
