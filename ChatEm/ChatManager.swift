2510761f0c9c74657b8f7b4da7db1b38bf5d136e
Noye Samuel <samuel.noye@amalitech.org>
2022-11-14 17:54:59 +0000

    update

--- ChatEm/ChatManager.swift
+++ ChatEm/ChatManager.swift
@@ -7,3 +7,14 @@
 import StreamChat
 import StreamChatUI
 import Foundation
+
+final class ChatManager {
+    static let shared = ChatManager()
+    
+    private var client: ChatClient!
+    
+    func setUp() {
+        let client = ChatClient(config: .init(apiKey: .init("69z8bzvavmcw")))
+        self.client = client
+    }
+}
