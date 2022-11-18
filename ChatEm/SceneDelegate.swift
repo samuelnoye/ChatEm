d4a8957984dcae499cd09793e183c7878aa1190a
Noye Samuel <samuel.noye@amalitech.org>
2022-11-18 10:19:51 +0000

    update

--- ChatEm/SceneDelegate.swift
+++ ChatEm/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let vc = ViewController()
+        let vc = LoginViewController()
         window.rootViewController = UINavigationController(rootViewController: vc)
         window.makeKeyAndVisible()
         self.window = window
