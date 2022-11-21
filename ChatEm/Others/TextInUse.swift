18591df87bc336d7cf37cb737f078fc3ac9ad249
Noye Samuel <samuel.noye@amalitech.org>
2022-11-21 12:36:20 +0000

    update

--- ChatEm/Others/TextInUse.swift
+++ ChatEm/Others/TextInUse.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+public struct TextsInUse {
+    public static let AppName = "ChatEm"
+    public static let Empty = ""
+    public static let One = "1"
+    public static let Two = "2"
+    public static let Three = "3"
+    public static let Four = "4"
+    public static let Five = "5"
+    public static let CountryCode = "+41"
+    public static let Slash = "/"
+    public static let Dash =  "-"
+    public static let Plus =  "+"
+    public static let Km =  "km"
+    public static let Cm =  "cm"
+    public static let Feet =  "feet"
+    public static let Kg =  "kg"
+    public static let Lbs =  "lbs"
+    public static let Kph =  "kph"
+    public static let Hr =  "hr"
+    public static let Dot =  "."
+    public static let Zero =  "0"
+    public static let Timeout =  ""
+    public static let Ok =  "Ok"
+    public static let Edit =  "edit"
+    public static let CreditSuccess =  "Your credit was purchased successfully."
+    //resources
+    public static let AppColor =  "AppColor"
+    public static let TextBGColor =  "TextBGColor"
+    //httpMethod
+    public static let PUT =  "PUT"
+}
